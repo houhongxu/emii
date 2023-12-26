e6841706b1dbb1dfd48ee8ee6097c9755f260b9e
HHX <903040380@qq.com>
2023-12-26 22:57:12 +0800

    feat: 初始化脚手架

---
+++ examples/client/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const Hello = () => {
+  const [text, setText] = React.useState('Hello Emi!')
+
+  return (
+    <span
+      onClick={() => {
+        setText('Hi!')
+      }}
+    >
+      {text}
+    </span>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+
+root.render(React.createElement(Hello))
